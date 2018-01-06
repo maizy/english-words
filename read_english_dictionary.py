3858dbeba1de45192260a1ab53017e5044c742c5
Nikita Kovaliov <nikita@maizy.ru>
2018-01-06 21:48:51 +0300

    return 32 random words in python example, port to python3

--- read_english_dictionary.py
+++ read_english_dictionary.py
@@ -1,16 +1,28 @@
-import json
-import os, sys
+#!/usr/bin/env python3
+import os
+import sys
+import random
+import functools
+import itertools
+
 
 def load_words():
-    try:
-        filename = os.path.dirname(sys.argv[0])+"\\"+"words_dictionary.json"
-        with open(filename,"r") as english_dictionary:
-            valid_words = json.load(english_dictionary)
-            return valid_words
-    except Exception as e:
-        return str(e)
+    filename = os.path.join(os.path.dirname(__file__), 'words_alpha.txt')
+    with open(filename, 'r') as english_dictionary:
+        return [line.rstrip('\n\r') for line in english_dictionary.readlines()]
+
 
 if __name__ == '__main__':
+    k = int(sys.argv[1]) if len(sys.argv) > 1 else 32
+    assert(k > 0)
+
     english_words = load_words()
-    # demo print
-    print(english_words["fate"])
+    sample = random.sample(english_words, k=k)
+
+    columns = 4
+    column_length = round(len(sample) / columns)
+    by_columns = [sample[i * column_length:(i + 1) * column_length] for i in range(columns)]
+    max_length = max(len(i) for i in sample)
+
+    for i in range(column_length):
+        print('   '.join(l[i].ljust(max_length, ' ') if i < len(l) else ' ' * max_length for l in by_columns))
